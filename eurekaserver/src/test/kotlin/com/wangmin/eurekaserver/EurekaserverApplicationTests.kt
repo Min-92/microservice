694c091a7f1838a53cc820b15c33b87c07119624
Minjae lee <0201.mj.lee@gmail.com>
2023-10-30 22:54:54 +0900

    feat: eureka server 추가

---
+++ eurekaserver/src/test/kotlin/com/wangmin/eurekaserver/EurekaserverApplicationTests.kt
@@ -0,0 +1,11 @@
+package com.wangmin.eurekaserver
+
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+// 	@Test
+// 	fun contextLoads() {
+// 	}
+}
